5aa273d36d980e53d676d2f8a16c1575fce4bf03
Fexoh <95289600+Fexoh@users.noreply.github.com>
2025-09-09 15:36:52 +0200

    Updated actions workflows (#7)
    * removed scripts
    
    * Updated actions workflows
    
    * Commented out unused provider resulting in error
    
    * Fixed formatting
    
    * Made workflow actually format tofu code instead of just checking
    
    * Added placeholder resource so tofu plan can pass
    
    * Added terraform lock to gitignore
    
    * Made the workflows actually run in the infra/ directory where the files live

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 1.6.0"
   backend "s3" {
-    bucket     = "tofu-state-backend"
-    key        = "opentofu.tfstate" # Path to the state file inside the S3 bucket
+    bucket   = "tofu-state-backend"
+    key      = "opentofu.tfstate"            # Path to the state file inside the S3 bucket
     endpoint = "fsn1.your-objectstorage.com" # Endpoint override
 
     region                      = "hcloud"
@@ -13,10 +13,10 @@ terraform {
   }
   required_providers {
     # Add providers as needed. For example:
-    hcloud  = { source = "hetznercloud/hcloud", version = ">= 1.48.0" }
+    # hcloud  = { source = "hetznercloud/hcloud", version = ">= 1.48.0" }
     # proxmox = { source = "telmate/proxmox", version = ">= 3.0.0" }
     # tls    = { source = "hashicorp/tls", version = ">= 4.0.0" }
-    # null   = { source = "hashicorp/null", version = ">= 3.0.0" }
+    null = { source = "hashicorp/null", version = ">= 3.0.0" }
     # local  = { source = "hashicorp/local", version = ">= 2.4.0" }
     # http   = { source = "hashicorp/http", version = ">= 3.4.0" }
     # random = { source = "hashicorp/random", version = ">= 3.6.0" }
@@ -33,8 +33,8 @@ terraform {
 # }
 
 # Example-resource (placeholder):
-# resource "null_resource" "example" {
-#   provisioner "local-exec" {
-#     command = "echo 'Hello Bootstrap'"
-#   }
-# }
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Hello Bootstrap'"
+  }
+}
