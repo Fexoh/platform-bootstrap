081bfa77f0fa4770b53d832550f72a118d70d38a
Fexoh <a.longo@hotmail.de>
2025-09-09 15:01:31 +0200

    Added placeholder resource so tofu plan can pass

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ terraform {
     # hcloud  = { source = "hetznercloud/hcloud", version = ">= 1.48.0" }
     # proxmox = { source = "telmate/proxmox", version = ">= 3.0.0" }
     # tls    = { source = "hashicorp/tls", version = ">= 4.0.0" }
-    # null   = { source = "hashicorp/null", version = ">= 3.0.0" }
+    null = { source = "hashicorp/null", version = ">= 3.0.0" }
     # local  = { source = "hashicorp/local", version = ">= 2.4.0" }
     # http   = { source = "hashicorp/http", version = ">= 3.4.0" }
     # random = { source = "hashicorp/random", version = ">= 3.6.0" }
@@ -33,8 +33,8 @@ terraform {
 # }
 
 # Example-resource (placeholder):
-# resource "null_resource" "example" {
-#   provisioner "local-exec" {
-#     command = "echo 'Hello Bootstrap'"
-#   }
-# }
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Hello Bootstrap'"
+  }
+}
