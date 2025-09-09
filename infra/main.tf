a607d10559e8048d5f44fd50de1f18decb697732
Fexoh <a.longo@hotmail.de>
2025-09-09 14:46:44 +0200

    Commented out unused provider resulting in error

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ terraform {
   }
   required_providers {
     # Add providers as needed. For example:
-    hcloud  = { source = "hetznercloud/hcloud", version = ">= 1.48.0" }
+    # hcloud  = { source = "hetznercloud/hcloud", version = ">= 1.48.0" }
     # proxmox = { source = "telmate/proxmox", version = ">= 3.0.0" }
     # tls    = { source = "hashicorp/tls", version = ">= 4.0.0" }
     # null   = { source = "hashicorp/null", version = ">= 3.0.0" }
