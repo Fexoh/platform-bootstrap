1b92e3ff43f1cc3d6d9a48a4a8afceed9c622880
Fexoh <a.longo@hotmail.de>
2025-09-10 19:07:37 +0200

    Fixed S3 endpoint

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket   = "tofu-state-backend"
     key      = "opentofu.tfstate"                                                                     # Path to the state file inside the S3 bucket
-    endpoint = "https://8705ca58998c25e90952292cfeb5eeae.r2.cloudflarestorage.com/tofu-state-backend" # Endpoint override
+    endpoint = "8705ca58998c25e90952292cfeb5eeae.r2.cloudflarestorage.com" # Endpoint override
 
     region                      = "weur"
     skip_credentials_validation = true
